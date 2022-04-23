aa87202c4a6a4b0dbe6b784443888d84b12e74d1
joseluisgomezcecena <joseluisgomezcecegna@gmail.com>
2022-04-23 15:23:09 -0700

    Added namespace to urls and added static files.

--- mysite/myapp/urls.py
+++ mysite/myapp/urls.py
@@ -17,8 +17,9 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 
+app_name = 'my_app'
 urlpatterns = [
     path('', views.index),
     path('products/', views.products),
-    path('products/<int:id>', views.product_detail),
+    path('products/<int:id>', views.product_detail, name='product_detail'),
 ]
